20467d9b83a791f86a48ebd887c98bf08f2fcfe1
SilverDeveloper <52933126+silver-the-dev@users.noreply.github.com>
2026-06-05 06:25:47 -0300

    first commit

---
+++ src/test/java/io/github/silver_the_dev/libraryapi/LibraryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.silver_the_dev.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
